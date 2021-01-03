6a660e62aec72e4c7421f4057b1dc411c7ac0dc5
bambamayo <ayobamiagunroye@gmail.com>
2021-01-03 13:32:41 +0100

    add about page, make responsive, adjustment to other pages mobile view

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/Layout"
 
 export default function Porfolio() {
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulAbout {
+        aboutText {
+          aboutText
+        }
+        aboutImage {
+          fluid(maxWidth: 1000) {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      <h2>about page</h2>
+      <section className="page-wrapper">
+        <h2 className="page-header">About me</h2>
+        <div className="about">
+          <div className="about__text-cont">
+            <p className="about__text">
+              {data.contentfulAbout.aboutText.aboutText}
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
